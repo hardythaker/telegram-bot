6381f50cd34390bf648a23b0d338bb8215aa0940
Thaker <hardik.vinod-thaker@capgemini.com>
2022-04-09 01:48:36 +0530

    Clean up and TODOs

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 //TODO: How to hide all the previous messages from a new joined users and show after verification
 //TODO: Instead of user_ID we can store telegram username as well in sheet, but what is user changes its user name
+//TODO: Time limt for all the messages from the bot
+//TODO: Varification failed message should updated the same greting message.
+//TODO: Need to provide some fallback mechanism if user verification gets failed multiple times.
+//TODO: LOW: Remove the google form data if user lefts the group
+//
 const { Telegraf } = require('telegraf');
 const {google} = require('googleapis');
-const {TOKEN, SCOPES, SPREADSHEETID, SHEETNAME, USER_ID_COLUMN, GOOGLE_FORM_ID} = require('./config')
+const {TOKEN, SCOPES, SPREADSHEETID, SHEETNAME, USER_ID_COLUMN, GOOGLE_FORM_ID, GET_GREETING_TEXT} = require('./config')
 
 const bot = new Telegraf(TOKEN);
-
-// bot.command('/allowedall',(ctx, next)=>{
-//     //console.log(ctx.from);
-//     bot.telegram.restrictChatMember('-1001769069041','910940623',{
-//         can_send_messages : true
-//     })
-// })
 
 bot.action('verify',async (ctx)=>{
     //console.log(ctx)
@@ -37,22 +35,14 @@ bot.action('verify',async (ctx)=>{
 
 bot.on('new_chat_members', async (ctx) => {
     try {
-        // console.log(ctx)
-        // console.log(ctx.from)
-        // console.log(ctx.chat)
-        // console.log(ctx.message.new_chat_participant)
-        // console.log(ctx.message.new_chat_member)
-        // console.log(ctx.message.new_chat_members)
         const chatId = ctx.chat.id
         const newMemberId = ctx.message.new_chat_member.id
-        const fromId = ctx.from.id
         const newMemberFirstName = ctx.message.new_chat_member.first_name
         const newMemberLastName = ctx.message.new_chat_member.last_name
-
+        const fullname = `${newMemberFirstName} ${newMemberLastName}`
         await setUserChatPermission(chatId, newMemberId, false);
         const url = `${GOOGLE_FORM_ID}${newMemberId}`
-        await bot.telegram.sendMessage(chatId, 
-            `Hi ${newMemberFirstName} ${newMemberLastName},\nWelcome to the group!\nKindly fill below google form to get access to groups chat.\nOnce done, Kindly click on verify button to get yourself verifed.`,
+        await bot.telegram.sendMessage(chatId, GET_GREETING_TEXT(fullname),
             {
                 reply_markup:{
                     inline_keyboard:[
@@ -86,11 +76,8 @@ async function getGoogleSheetData(user_id){
         scopes: SCOPES
     })
     const client = await auth.getClient();
-    const sheets = google.sheets({version: 'v4', auth: client});
     
-    // const metadata = await sheets.spreadsheets.get({
-    //     spreadsheetId: SPREADSHEETID
-    // })
+    const sheets = google.sheets({version: 'v4', auth: client});
 
     const data = await sheets.spreadsheets.values.get({
         auth,
@@ -106,49 +93,3 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
-// const approvedUsers = [];
-
-// bot.on('message',(ctx)=>{
-//     console.log(ctx)
-//     console.log(ctx.chat.id)
-//     const fullName = (ctx.from.first_name + ctx.from.last_name).toLocaleLowerCase()
-//     const localFullName = (users[0].first_name + users[0].last_name).toLocaleLowerCase()
-//     if(fullName === localFullName && users.findIndex(x=> x.first_name == )){
-//         return
-//     }
-//     //bot.telegram.sendMessage(ctx.chat.id,"You are not authenticed user!!")
-// })
-
-// bot.command('link',(ctx, next) => {
-//     console.log(ctx.message.chat)
-//     bot.telegram.sendMessage(ctx.chat.id, "Kindly click on below button to generate your google form link",{
-//         reply_markup:{
-//             inline_keyboard:[
-//                 [
-//                     {text:"Form", callback_data: 'sendGoogleFormLink'}
-//                 ],
-//             ]
-//         }
-//     })
-//     // bot.telegram.sendPhoto(
-//     //     '-1001769069041',
-//     //     ctx.message.photo[ctx.message.photo.length - 1].file_id,
-//     //     {
-//     //         reply_markup:{
-//     //             inline_keyboard:[
-//     //                 [
-//     //                     {text:"Approve", callback_data: 'approve'}
-//     //                 ],
-//     //                 [
-//     //                     {text:"Reject", callback_data: 'reject'}
-//     //                 ]
-//     //             ]
-//     //         }
-//     //     }
-//     // )
-// })
-
-// bot.start((ctx=>{
-//     bot.telegram.sendMessage(ctx.chat.id,"Upload your image")
-// }))
