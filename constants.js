cfd28fa287e4847683d4e785ce86b78b46738682
Thaker <hardik.vinod-thaker@capgemini.com>
2022-04-19 22:25:37 +0530

    Added .env support, Listen only on Private Chat, Error Handling and Code Refactoring

---
+++ constants.js
@@ -0,0 +1,29 @@
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const SHEET_NAME="Form Responses 2"
+const COLUMNS="A:D"
+const LINK_EXPIRES_IN=1 //minutes
+const GROUP_ID = "-1001769069041"
+
+const GET_GREETING_TEXT = (fullname) => `Hi ${fullname}
+Welcome to the group!
+Kindly fill below google form to get access to groups chat.
+Once done, Kindly click on verify button to get yourself verifed.`
+
+const IMAGE_TEXT = `Thank you for filling out the form,
+\nKindly Send the photo of the first page of your agrement.
+\nPlease note that first page of agreement does not contain any of your personal infomation. We are collecting this information so that outsider cannot join the our group and your information realted to you and your flat will be safe and only accessible amongst the other owners. Please make sure you are only uploading first page of agreement and not other pages.
+\n<b>Note:</b> This image will be further verified by the group admins, if you're sending anything else other than first page of the agreement, your request will be rejected!!!`
+
+const VARIFICATION_FAILED_TEXT = `Verification Failed.\nPlease make sure you have filled the form correctly and try again.
+\nIf you are still facing the issues, kindly type /help in this chat.`
+
+const CHAT_JOIN_REQUEST_TEXT= `Thanks for providing the image!!!\n\nKindly click on below button to request to join the group
+\n<b>Note:</b> Link is valid only for 1 minute`
+
+const GET_CAPTION_TEXT = (sessionRow) => `@admin\nGoogle Form Data
+UserId: ${sessionRow[3]}
+Name: ${sessionRow[1]} ${sessionRow[2]}
+`
+
+module.exports = { SCOPES, SHEET_NAME, COLUMNS, LINK_EXPIRES_IN, 
+    GET_GREETING_TEXT, GROUP_ID, IMAGE_TEXT, VARIFICATION_FAILED_TEXT, CHAT_JOIN_REQUEST_TEXT, GET_CAPTION_TEXT}
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 //TODO: Instead of user_ID we can store telegram username as well in sheet, but what if user changes its user name
 //TODO: Time limt for all the messages from the bot
-//TODO: Varification failed message should updated the same greting message.
 //TODO: Need to provide some fallback mechanism if user verification gets failed multiple times.
 //TODO: LOW: Remove the google form data if user lefts the group
-//TODO: Option to set that which verification method to be used.
-//TODO: Let user know that there Request is approved or not. and provide fallback
+//TODO: LOW: Option to set that which verification method to be used.
+//TODO: Let user know that there Request is approved or not. and provide fallback.
+//TODO: FIXED: Bot Should not act or read on the messages sent in the group. //Fixed for /start and Photo Event on which bot reacts.
+//TODO: Remove await where ever required and test. lets try with the methods which are not using any variables
+//TODO: LOW: Error logging to a seprate admin group.
+//TODO: TESTING on multiple devices at same time. Working so far with 2 simultanious requests
+//TODO: Integrate Winston with Event, UserID, Time format
+//TODO: Webpack to min the script
+//TODO: PROD - DEV diffrentiate to run express or not
+//TODO: Need to clear the session after rejecting the Request
 
 //Only for development browser testing
 const express = require('express');
@@ -19,18 +26,15 @@ let DATA;
 const { Telegraf } = require('telegraf');
 const LocalSession = require('telegraf-session-local')
 const {google} = require('googleapis');
-const {TOKEN, SCOPES, SPREADSHEETID, SHEETNAME, USER_ID_COLUMN, 
-    GOOGLE_FORM_ID, LINK_EXPIRES_IN, GET_GREETING_TEXT, GROUP_ID,
-    IMAGE_TEXT, VARIFICATION_FAILED_TEXT, CHAT_JOIN_REQUEST_TEXT,
-    GET_CAPTION_TEXT} = require('./config');
+const {SCOPES, SHEETNAME, COLUMNS, LINK_EXPIRES_IN, GET_GREETING_TEXT, GROUP_ID,
+    IMAGE_TEXT, VARIFICATION_FAILED_TEXT, CHAT_JOIN_REQUEST_TEXT, GET_CAPTION_TEXT} = require('./constants');
 
-const bot = new Telegraf(TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const localSession = new LocalSession({ 
     database: 'example_db.json',
     getSessionKey : (ctx) => ctx.from.id
 })
-bot.use(localSession.middleware())
 
 const emoji = {
     back : "\u{1F519}",
@@ -41,91 +45,106 @@ const emoji = {
 
 const approveRejectKeyboard = {
     inline_keyboard:[
+        [{text:`${emoji.checkMark} Approve`, callback_data: 'approve'}],
+        [{text:`${emoji.crossmark} Reject`, callback_data: 'reject'}]
+    ]
+}
+
+const confirmRejectGoBackKeyboard = {
+    inline_keyboard:[
         [
-            {text:`${emoji.checkMark} Approve`, callback_data: 'approve'}
+            {text:'Confirm Reject? Yes', callback_data:'confirmReject' }
         ],
         [
-            {text:`${emoji.crossmark} Reject`, callback_data: 'reject'}
+            {text: `${emoji.back} Go Back`, callback_data:'goBack' }
         ]
     ]
 }
 
 bot.start(async (ctx) => {
     console.log('Start')
-    browserLog("start",ctx)
+    browserLog("Start", ctx)
 
+    //To avoid listening commands or messages in group chat
+    if(ctx.chat.type != 'private') return null 
     try {
-        const chatId = ctx.chat.id
-        const newMemberId = ctx.from.id
-        const newMemberFirstName = ctx.from.first_name
-        const newMemberLastName = ctx.from.last_name || ''
-        const fullname = `${newMemberFirstName} ${newMemberLastName}`
-        const url = `${GOOGLE_FORM_ID}${newMemberId}`
+        const ctxFrom = ctx.from
+        const fullname = `${ctxFrom.first_name} ${ctxFrom.last_name || ''}`
+        const url = `${process.env.GOOGLE_FORM_URL}${ctxFrom.id}`
 
-        //await setUserChatPermission(chatId, newMemberId, false);
-        await bot.telegram.sendMessage(chatId, GET_GREETING_TEXT(fullname),
-        {
+        await ctx.reply(GET_GREETING_TEXT(fullname),{
             reply_markup:{
                 inline_keyboard:[
-                    [
-                        {text:'Form', url }
-                    ],
-                    [
-                        {text:`${emoji.magnify} Verify`, callback_data:'verify' }
-                    ]
+                    [{text:'Form', url }],
+                    [{text:`${emoji.magnify} Verify`, callback_data:'verify' }]
                 ]
             },
             parse_mode: 'HTML',
-            // reply_to_message_id: ctx.message.message_id,
-            // allow_sending_without_reply: true
         })
     } catch (e) {
-        await bot.telegram.sendMessage(e.on.payload.chat_id, e.response.description)
         console.error(e);
+        await ctx.reply(e.response.description)
     }
 });
 
-//This fn is to verify that the user has filled the google form or not.
+bot.use(localSession.middleware())
+
+//Verify that the user has filled the google form or not.
 bot.action('verify',async (ctx)=>{
     console.log('verify')
-    //const name = `${cq.from.first_name} ${cq.from.last_name}`;
-    //const chatId = cq.message.chat.id;
+    browserLog("Verify", ctx)
     try {
-        const googleSheetRow = await getGoogleSheetData(ctx.callbackQuery.from.id)
-        ctx.session.formData = googleSheetRow
+        const formData = await verifyAndGetFormData(ctx.callbackQuery.from.id)
 
-        if(googleSheetRow){
+        //User's Form data is required while sending out the image in group with the caption
+        //That caption will contain the name, userid etc extracted from this formData.
+        ctx.session.formData = formData
+
+        if(formData){
             await ctx.editMessageText(IMAGE_TEXT,{parse_mode:'HTML'})
             await ctx.answerCbQuery()
-            //await bot.telegram.editMessageText(chatId,cq.message.message_id,"",mess)
         }
         else{
             await ctx.answerCbQuery(VARIFICATION_FAILED_TEXT,{show_alert: true})
-            //await ctx.answerCbQuery(chatId,)
         }
     } catch (e) {
         console.error(e);
+        await ctx.reply(e.response.description)
     }
 })
 
+//Once Received Photo from the user, send a chat invite link
 bot.on('photo',async (ctx)=>{
+    //To avoid listening commands or messages in group chat
+    if(ctx.chat.type != 'private') return null 
     console.log('photo');
     browserLog("Photo",ctx)
+    try{
+        //To avoid user uploading the image even if the form fillup and verification is not done.
+        if(!ctx.session.formData){
+            return ctx.reply("Alert!!!\n\nKindly first fill the form, click on verify button and then upload the image.")
+        }
+        
+        //Contains the photo url uploaded by the client  
+        const photos = ctx.message.photo
+        //Photo URL will be required later, once user creates a Group Joining Request via bot.
+        ctx.session.photoURL = photos[photos.length - 1].file_id;
 
-    ctx.session.photoURL = ctx.message.photo[ctx.message.photo.length - 1].file_id;
-    const link = await generateChatInviteLink(ctx.from.id, ctx.from.first_name)
+        const link = await generateChatInviteLink(ctx.from.id, ctx.from.first_name)
 
-    browserLog("Link",link)
-    await bot.telegram.sendMessage(ctx.chat.id, CHAT_JOIN_REQUEST_TEXT,{
-        reply_markup:{
-            inline_keyboard:[
-                [
-                    {text:`Join Group`, url:link.invite_link }
+        await ctx.reply(CHAT_JOIN_REQUEST_TEXT,{
+            reply_markup:{
+                inline_keyboard:[
+                    [{text:`Join Group`, url:link.invite_link }]
                 ]
-            ]
-        },
-        parse_mode: 'HTML'
-    })
+            },
+            parse_mode: 'HTML'
+        })
+    }
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
+    }
 })
 
 //Once the user click on "Join Group" btn, It will trigger this event.
@@ -134,80 +153,116 @@ bot.on('chat_join_request', async(ctx)=>{
     console.log('chat_join_request');
     browserLog("chat_join_request",ctx);
 
-    await ctx.revokeChatInviteLink(ctx.chatJoinRequest.invite_link.invite_link);
+    try{
+        //Revoke the link, so that other user cannot make request using same link
+        await ctx.revokeChatInviteLink(ctx.chatJoinRequest.invite_link.invite_link);
 
-    const session = ctx.session;
-    await ctx.replyWithPhoto(
-        session.photoURL,
-        {
-            reply_markup:approveRejectKeyboard,
-            caption: GET_CAPTION_TEXT(session.formData),
-            protect_content: true
-        }
-    )
-})
-
-bot.action(['approve','reject'],async(ctx,next)=>{
-    console.log("middleware",ctx.callbackQuery.data);
-    const admins = await ctx.getChatAdministrators()
-    browserLog(`Admins - ${ctx.callbackQuery.data}`,admins)
-    if(admins.some(x=> x.user.id == ctx.callbackQuery.from.id)){
-        return next(ctx);
+        //Extract the photoUrl and formData from the users' session so that it can be added into the image's caption
+        const session = ctx.session;
+        await ctx.replyWithPhoto(
+            session.photoURL,
+            {
+                reply_markup:approveRejectKeyboard,
+                caption: GET_CAPTION_TEXT(session.formData),
+                protect_content: true
+            }
+        )
     }
-    ctx.answerCbQuery("Only Admins are allowed to perform this operation!!!",{
-        show_alert: true
-    })
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
+    }
 })
 
+//Check if the 'Approve' and 'Reject' events are indeed performed by admins. Alert others
+bot.action(['approve','reject'], async(ctx,next)=>{
+    console.log("middleware",ctx.callbackQuery.data);
+    try{
+        const admins = await ctx.getChatAdministrators()
+        //browserLog(`Admins - ${ctx.callbackQuery.data}`,admins)
+        
+        //If request is from any admin, let it pass to next middleware
+        if(admins.some(x=> x.user.id == ctx.callbackQuery.from.id)){
+            return next(ctx);
+        }
+
+        //If request is not from admins, show the alert.
+        ctx.answerCbQuery("Only Admins are allowed to perform this operation!!!",{
+            show_alert: true
+        })
+
+    }
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
+    }
+})
+
+//Approve the user's joining request
 bot.action('approve', async(ctx)=>{
     console.log('Approve');
     browserLog("Approved Request", ctx)
     
-    const cq = ctx.callbackQuery;
-    const caption = cq.message.caption;
-    const name = `${cq.from.first_name} ${cq.from.last_name}`
-    const userId = getUserIdFromCaption(caption);
-    if(await ctx.approveChatJoinRequest(userId)){
-        await ctx.editMessageCaption(`${caption}\nApproved by ${name}`,{
-            reply_markup:{}
-        })
+    try{
+        const cq = ctx.callbackQuery;
+        const caption = cq.message.caption;
+        const name = `${cq.from.first_name} ${cq.from.last_name || ''}`
+        const userId = getUserIdFromCaption(caption);
+
+        if(await ctx.approveChatJoinRequest(userId)){
+            await ctx.editMessageCaption(`${caption}\nApproved by ${name}`,{reply_markup:{}})
+        }
+        else{
+            ctx.answerCbQuery('Error: User aprroval failed!!!',{show_alert: true});
+        }
     }
-    else{
-        ctx.reply('Error: User aprroval failed!!!')
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
     }
-    await bot.telegram.answerCbQuery(cq.id)
+    finally{
+        ctx.answerCbQuery();
+    }
 })
 
+//Show confirmRejectGoBackKeyboard as a confirmation
 bot.action('reject', async(ctx)=>{
     console.log('reject');
     browserLog("Reject Request",ctx);
     
-    const cq = ctx.callbackQuery;
-    const caption = cq.message.caption;
-    await ctx.editMessageCaption(`${caption}`,{
-        reply_markup:{
-            inline_keyboard:[
-                [
-                    {text:'Confirm Reject? Yes', callback_data:'confirmReject' }
-                ],
-                [
-                    {text: `${emoji.back} Go Back`, callback_data:'goBack' }
-                ]
-            ]
-        },
-        parse_mode: 'HTML'
-    })
-    await ctx.answerCbQuery();
+    try{
+        await ctx.editMessageCaption(`${ctx.callbackQuery.message.caption}`,{
+            reply_markup:confirmRejectGoBackKeyboard,
+            parse_mode: 'HTML'
+        })
+    }
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
+    }
+    finally{
+        await ctx.answerCbQuery();
+    }
 })
 
+//Provide back functionality after Reject Confirmation
 bot.action('goBack', async (ctx)=>{
     console.log("goback");
     browserLog("Go Back",ctx)
 
-    await ctx.editMessageCaption(ctx.callbackQuery.message.caption,{reply_markup: approveRejectKeyboard,parse_mode:'HTML'})
-    await ctx.answerCbQuery()
+    try{
+        await ctx.editMessageCaption(ctx.callbackQuery.message.caption,{reply_markup: approveRejectKeyboard,parse_mode:'HTML'})
+    }
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
+    }
+    finally{
+        await ctx.answerCbQuery()
+    }
 })
 
+//Reject the user's Chat Joining Request
 bot.action('confirmReject', async(ctx)=>{
     //await ctx.declineChatJoinRequest()
     const cq = ctx.callbackQuery;
@@ -215,48 +270,60 @@ bot.action('confirmReject', async(ctx)=>{
     const name = `${cq.from.first_name} ${cq.from.last_name}`;
     const userId = getUserIdFromCaption(caption);
 
-    if(await ctx.declineChatJoinRequest(userId)){
-        console.log(ctx.session)
-        await ctx.editMessageCaption(`${caption}\nRejected by ${name}`,{reply_markup:{}})
+    try{
+        if(await ctx.declineChatJoinRequest(userId)){
+            return await ctx.editMessageCaption(`${caption}\nRejected by ${name}`,{reply_markup:{}})
+            
+        }
+        ctx.answerCbQuery('Error: User Rejection failed!!!',{show_alert: true});
     }
-    else{
-        ctx.reply('Error: User aprroval failed!!!')
+    catch(e){
+        console.error(e);
+        await ctx.reply(e.response.description)
     }
-    await bot.telegram.answerCbQuery(cq.id)
+    finally{
+        await ctx.answerCbQuery()
+    }
 })
 
 //Once the user is approved and added to the group, It will trigger this event.
 bot.on('new_chat_members', async (ctx) => {
     console.log('new_chat_members');
     browserLog('new_chat_members',ctx)
+    
+    const formData = ctx.session.formData;
+    //Clear the user session since user has joined the group
+    ctx.telegram.sendMessage(formData[formData.length-1],'Your Request to Join the group has been Approved!!!')
     ctx.session = null
 })
 
 //This will Run on both Join as well as Left Event
-bot.on('chat_member',async (ctx)=>{
-    console.log('chat_member ' + ctx.chatMember.new_chat_member.status);
-    browserLog('chat_member ' + ctx.chatMember.new_chat_member.status,ctx)
-})
+// bot.on('chat_member',async (ctx)=>{
+//     console.log('chat_member ' + ctx.chatMember.new_chat_member.status);
+//     browserLog('chat_member ' + ctx.chatMember.new_chat_member.status,ctx)
+// })
 
-async function setUserChatPermission(chatId, userId, sendPermission){
-    await bot.telegram.restrictChatMember(chatId,userId,{
-        can_send_messages : sendPermission
-    })
-}
+// async function setUserChatPermission(chatId, userId, sendPermission){
+//     await bot.telegram.restrictChatMember(chatId,userId,{
+//         can_send_messages : sendPermission
+//     })
+// }
 
-async function getGoogleSheetData(user_id){
+//Fecth data from google sheets and verify if the user is exists or not
+async function verifyAndGetFormData(user_id){
     const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: SCOPES
+        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        scopes: SCOPES,
     })
+    auth.fromJSON
     const client = await auth.getClient();
     
     const sheets = google.sheets({version: 'v4', auth: client});
 
     const data = await sheets.spreadsheets.values.get({
         auth,
-        spreadsheetId: SPREADSHEETID,
-        range:`${SHEETNAME}!${USER_ID_COLUMN}`
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range:`${SHEETNAME}!${COLUMNS}`
     })
     const row = data.data.values.find(x=> x.some(y => y == user_id));
     console.log(row);
@@ -267,7 +334,6 @@ async function generateChatInviteLink(user_id, name){
     return await bot.telegram.createChatInviteLink(GROUP_ID,{
         name: `${user_id} ${name}`,
         creates_join_request: true,
-        //member_limit: 1,
         expire_date: Math.floor((Date.now()/1000) + (LINK_EXPIRES_IN*60))
     })
 }
@@ -281,13 +347,19 @@ function getUserIdFromCaption(caption){
     else{
         console.log('Error: Unable to find User Id!!!')
     }
-    console.log(userId);
     return userId;
 }
 
 bot.launch({
     allowedUpdates:['chat_member','callback_query','chat_join_request','message']
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//     bot = new TelegramBot(token);
+//     bot.setWebHook(process.env.HEROKU_URL + bot.token);
+// } else {
+//     bot = new TelegramBot(token, { polling: true });
+// }
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
@@ -308,8 +380,7 @@ JSON.safeStringify = (obj, indent = 2) => {
     );
     cache = null;
     return retVal;
-  };
-
+};
 
 const browserLog = (key, data) => { 
     LOGS.push({[key]: data});
